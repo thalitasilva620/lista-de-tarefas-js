b4d6917833907a1448de6bd7b34b5669fdaf01f3
Thalita Silva <thalitasilva687@gmail.com>
2026-01-29 13:37:39 -0300

    feat; estrutura inicial do projeto

---
+++ README.md
@@ -0,0 +1,30 @@
+# 📋 Lista de Tarefas - JavaScript
+
+Projeto simples desenvolvido para praticar conceitos básicos de JavaScript, como:
+
+- Arrays
+- Manipulação do DOM
+- Renderização dinâmica
+- Eventos (addEventListener)
+
+## 🚀 Funcionalidades
+- Adicionar tarefas
+- Renderizar lista dinamicamente no DOM
+
+## 🛠️ Tecnologias utilizadas
+- HTML
+- CSS
+- JavaScript (Vanilla)
+
+## 📚 Objetivo
+Este projeto faz parte do meu processo de aprendizado em JavaScript.
+A ideia é evoluir o projeto aos poucos, adicionando novas funcionalidades
+conforme avanço nos estudos.
+
+## 🔄 Próximas melhorias
+- Remover tarefas
+- Marcar tarefas como concluídas
+- Salvar tarefas no localStorage
+
+---
+Desenvolvido por Thalita Silva 💙
---
+++ script.js
@@ -0,0 +1,34 @@
+const tarefas = [
+    "Estudar JavaScript",
+    "Fazer exercícios de programação",
+    "Ler artigos sobre desenvolvimento web",
+    "Praticar projetos práticos"
+]
+
+const input = document.getElementById('inputTarefa');
+const btnAdicionar = document.getElementById('btnAdicionar');
+const listaTarefas = document.getElementById('listaTarefas');
+
+function renderizarTarefas() {
+    listaTarefas.innerHTML = "";
+
+    tarefas.forEach((tarefa) => {
+        const li = document.createElement('li');
+        li.textContent = tarefa;
+        listaTarefas.appendChild(li);
+    });
+}
+
+btnAdicionar.addEventListener('click', () => {
+    const texto = input.value;
+
+    if (texto === "") {
+        alert("Digite uma tarfea!");
+        return;
+    }
+
+    tarefas.push(texto);
+    input.value = "";
+
+    renderizarTarefas();
+});
