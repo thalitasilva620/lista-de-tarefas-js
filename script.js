59831ba554bd7ca4557a089487953481a5e17401
Thalita Silva <thalitasilva687@gmail.com>
2026-02-09 19:10:55 -0300

    style: melhora visual e acessibilidade do botão remover

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const tarefas = [
-    "Estudar JavaScript",
-    "Fazer exercícios de programação",
-    "Ler artigos sobre desenvolvimento web",
-    "Praticar projetos práticos"
-]
+const tarefas = [];
 
 const input = document.getElementById('inputTarefa');
 const botao = document.getElementById('btnAdicionar');
@@ -11,26 +6,36 @@ const listaTarefas = document.getElementById('listaTarefas');
 const mensagemVazia = document.getElementById('mensagemVazia');
 const contadorTarefas = document.getElementById("contadorTarefas");
 
+function salvarTarefas() {
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+}
+
+function carregarTarefas() {
+  const tarefasSalvas = localStorage.getItem("tarefas");
+
+  if (tarefasSalvas) {
+    tarefas.push(...JSON.parse(tarefasSalvas));
+  }
+}
+
 
 function renderizarTarefas() {
     listaTarefas.innerHTML = "";
 
-    if (tarefas.length === 0) {
-        mensagemVazia.style.display = "block";
-    } else {
-        mensagemVazia.style.display = "none";
-    }
-
     mensagemVazia.style.display = tarefas.length === 0 ? "block" : "none";
 
     const total = tarefas.length;
-    const concluidas = tarefas.filter(tarefa => tarefa.concluida).length;
+    const concluidas = tarefas.filter(t => t.concluida).length;
 
     contadorTarefas.textContent = `Total: ${total} | Concluídas: ${concluidas}`;
 
     tarefas.forEach(function (tarefa, index) {
         const li = document.createElement('li');
-        li.textContent = tarefa.texto;
+
+        const spanTexto = document.createElement("span");
+        spanTexto.textContent = tarefa.texto;
+
+        li.appendChild(spanTexto);
 
         if (tarefa.concluida) {
             li.classList.add("concluída");
@@ -43,7 +48,10 @@ function renderizarTarefas() {
         })
 
         const botaoRemover = document.createElement("button");
-        botaoRemover.textContent = "❌";
+        botaoRemover.textContent = "";
+
+        botaoRemover.classList.add("botao-remover");
+        botaoRemover.setAttribute("aria-label", "Remover tarefa");
 
         botaoRemover.addEventListener('click', function (event) {
             event.stopPropagation();
@@ -60,14 +68,11 @@ function renderizarTarefas() {
 botao.addEventListener('click', () => {
     const texto = input.value;
 
-    if (texto === "") {
-        alert("Digite uma tarfea!");
-        return;
-    }
+    if (texto === "") return;
 
-    tarefas.push(texto);
+    tarefas.push({texto, concluida: false});
     input.value = "";
-
+    salvarTarefas();
     renderizarTarefas();
 
 });
@@ -77,3 +82,6 @@ input.addEventListener('keydown', function (event) {
         botao.click();
     }
 });
+
+carregarTarefas();
+renderizarTarefas();
